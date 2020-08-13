1d74c9118330bed34b3d98d820eb20e5adfe251d
Gonz Tirado <tamudo84@gmail.com>
2020-08-13 12:16:30 +0200

    Added a Google Vision sample for text recognition

--- app/src/main/java/com/architectcoders/grupo2verano2020/OcrActivity.kt
+++ app/src/main/java/com/architectcoders/grupo2verano2020/OcrActivity.kt
@@ -1,11 +1,137 @@
 package com.architectcoders.grupo2verano2020
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.SurfaceHolder
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.text.TextBlock
+import com.google.android.gms.vision.text.TextRecognizer
+import kotlinx.android.synthetic.main.activity_ocr.*
+import java.lang.Exception
+import kotlin.properties.Delegates
 
 class OcrActivity : AppCompatActivity() {
+    private var mCameraSource by Delegates.notNull<CameraSource>()
+    private var textRecognizer by Delegates.notNull<TextRecognizer>()
+
+    private val PERMISSION_REQUEST_CAMERA = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ocr)
+
+        startCameraSource()
+    }
+
+    private fun startCameraSource() {
+
+        //  Create text Recognizer
+        textRecognizer = TextRecognizer.Builder(this).build()
+
+        if (!textRecognizer.isOperational) {
+            Toast.makeText(
+                this,
+                "Dependencies are not loaded yet...please try after few moment!!",
+                Toast.LENGTH_SHORT
+            ).show()
+            Log.d("Debug", "Dependencies are downloading....try after few moment")
+            return
+        }
+
+        //  Init camera source to use high resolution and auto focus
+        mCameraSource = CameraSource.Builder(applicationContext, textRecognizer)
+            .setFacing(CameraSource.CAMERA_FACING_BACK)
+            .setRequestedPreviewSize(1280, 1024)
+            .setAutoFocusEnabled(true)
+            .build()
+
+        surfaceCameraPreview.holder.addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+                // Do no nothing
+            }
+
+            override fun surfaceDestroyed(p0: SurfaceHolder) {
+                mCameraSource.stop()
+            }
+
+            @SuppressLint("MissingPermission")
+            override fun surfaceCreated(p0: SurfaceHolder) {
+                try {
+                    if (isCameraPermissionGranted()) {
+                        mCameraSource.start(surfaceCameraPreview.holder)
+                    } else {
+                        requestForPermission()
+                    }
+                } catch (e: Exception) {
+                    // Toast.makeText(this, "Error:  ${e.message}", Toast.LENGTH_SHORT).show()
+                    Log.i("info", "Error:  ${e.message}")
+                }
+            }
+        })
+
+        textRecognizer.setProcessor(object : Detector.Processor<TextBlock> {
+            override fun release() {}
+
+            override fun receiveDetections(detections: Detector.Detections<TextBlock>) {
+                val items = detections.detectedItems
+
+                if (items.size() <= 0) {
+                    return
+                }
+
+                textViewResult.post {
+                    val stringBuilder = StringBuilder()
+                    for (i in 0 until items.size()) {
+                        val item = items.valueAt(i)
+                        stringBuilder.append(item.value)
+                        stringBuilder.append("\n")
+                    }
+                    textViewResult.text = stringBuilder.toString()
+                }
+            }
+        })
+    }
+
+    fun isCameraPermissionGranted(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            this,
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestForPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.CAMERA),
+            PERMISSION_REQUEST_CAMERA
+        )
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode != PERMISSION_REQUEST_CAMERA) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+            return
+        }
+
+        if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (isCameraPermissionGranted()) {
+                mCameraSource.start(surfaceCameraPreview.holder)
+            } else {
+                Toast.makeText(this, "Permission need to grant", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
     }
 }
