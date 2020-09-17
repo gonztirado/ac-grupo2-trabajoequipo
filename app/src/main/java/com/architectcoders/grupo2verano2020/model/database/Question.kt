4c5be89148aafac56bb4863551b047bdb81b8dca
Ricardo <manzananaranja1486@gmail.com>
2020-09-17 17:23:50 +0200

    ROOM -Se introdujo room sin error

--- app/src/main/java/com/architectcoders/grupo2verano2020/model/database/Question.kt
+++ app/src/main/java/com/architectcoders/grupo2verano2020/model/database/Question.kt
@@ -1,30 +1,20 @@
 package com.architectcoders.grupo2verano2020.model.database
 
 
+import androidx.room.Embedded
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.architectcoders.grupo2verano2020.model.server.Answers
+import com.architectcoders.grupo2verano2020.model.server.QuestionsAnswers
 
 
 //todo: Uid = Jwf0Y4VAFsXOm8hBRP6f
 
+//esta es la identidad
 @Entity
-data class Uid(
-    val id: String,
-    val question: List<Questions>,
-    val questionCount:Int,
-    val quiz:String
-)
-
-
-@Entity
-data class Questions(
-    val id: String,
-    val question: String,
-    val answer: List<Answers>
-)
-
-@Entity
-data class Answers(
-    val answer: String,
-    val isCorrect: Boolean
+data class QuestionsAnswers(
+    @PrimaryKey val id: String,
+    val question: String?,
+    @Embedded val answer: ArrayList<Answers>
 )
 
--- app/src/main/java/com/architectcoders/grupo2verano2020/model/database/QuestionDao.kt
+++ app/src/main/java/com/architectcoders/grupo2verano2020/model/database/QuestionDao.kt
@@ -5,16 +5,22 @@ import androidx.room.*
 
 @Dao
 interface QuestionDao {
-    @Query("SELECT * FROM Uid")
-    fun getAll(): List<Uid>
+    @Query("SELECT * FROM QuestionsAnswers")
+    fun getAll(): List<QuestionsAnswers>
 
-    @Query("SELECT *FROM uid WHERE id= :id")
-    fun findById(id: String): Uid
+    @Query("SELECT *FROM QuestionsAnswers WHERE id= :id")
+    fun findById(id: String): QuestionsAnswers
+
+
+    @Query("SELECT COUNT(id) FROM QuestionsAnswers")
+    fun questionCount(): Int
+
+
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    fun insertUid(uid: List<Uid>)
+    fun insertUid(uid: List<QuestionsAnswers>)
 
     @Update
-    fun updateUid(uid: Uid)
+    fun updateUid(uid: QuestionsAnswers)
 
 }
